cc2bbb8d19d9f5c8f7f4974a8a721d236843ee5d
JaredHGray <94664698+JaredHGray@users.noreply.github.com>
2023-03-04 15:11:01 -0700

    completed Game class in JS

--- play.js
+++ play.js
@@ -33,3 +33,53 @@ class Button {
     });
   }
 }
+
+class Game {
+    buttons;
+    allowPlayer;
+    sequence;
+    playerPlaybackPos;
+    mistakeSound;
+
+    constructor() {
+        this.buttons = new Map();
+        this.allowPlayer = false;
+        this.sequence = [];
+        this.playerPlaybackPos = 0;
+        this.mistakeSound = loadSound('error.mp3');
+
+        document.querySelectorAll('.game-button').forEach((element, i) => {
+            if(i < btnDescriptions.length){
+                this.buttons.set(element.id, new Button(btnDescriptions[i], element));
+            }
+        });
+
+        const playerNameEl = document.querySelector('.player-name');
+        playerNameEl.textContent = this.getPlayerName();
+    }
+
+    async pressButton(button){
+        if(this.allowPlayer){
+            this.allowPlayer = false;
+            await this.buttons.get(button.id).press(1.0);
+
+            if(this.sequence[this.playerPlaybackPos].element.id ===button.id){
+                this.playerPlaybackPos++;
+                if(this.playerPlaybackPos === this.sequence.length){
+                    this.playerPlaybackPos = 0;
+                    this.addButton();
+                    this.updateScore(this.sequence.length-1);
+                    await this.playSequence();
+                }
+                this.allowPlayer = true;
+            }
+            else{
+                this.saveScore(this.sequence.length-1);
+                this.mistakeSound.play();
+                await this.buttonDance(2);
+            }
+        }
+    }
+
+    
+}
